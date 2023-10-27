81a6d803a693a6542bc5d21f2daff45d43b756fb
Charles Quinn <charles.quinn@planfile.com>
2023-10-27 10:46:21 -0400

    Added a frontend with several components.

--- frontend/dashboard/src/app/app.module.ts
+++ frontend/dashboard/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { CrewsComponent } from './components/crews/crews.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    CustomersComponent,
+    JobsComponent,
+    TasksComponent,
+    CrewsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
