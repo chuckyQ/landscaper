b39843016487df358b3588fd7a90168cd48b2bee
Charles Quinn <charles.quinn@planfile.com>
2023-10-27 21:22:25 -0400

    Added some code to create a mockup for the customer table

--- frontend/dashboard/src/app/components/customers/customers.component.ts
+++ frontend/dashboard/src/app/components/customers/customers.component.ts
@@ -1,4 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AddCustomerModalComponent } from '../add-customer-modal/add-customer-modal.component';
+
+
+interface Customer {
+  address: string
+  city: string
+  state: string
+  zipCode: string
+  phoneNumber: string
+  contactName: string
+}
+
+
+function createCustomer(address: string, city: string, state: string,
+  zipCode: string, phoneNumber: string, contactName: string) {
+  let c: Customer = {
+    address: address,
+    city: city,
+    state: state,
+    zipCode: zipCode,
+    phoneNumber: phoneNumber,
+    contactName: contactName,
+  }
+
+  return c
+}
+
 
 @Component({
   selector: 'app-customers',
@@ -7,9 +35,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
-  constructor() { }
+  customers: Customer[]
+
+  constructor(public modal: NgbModal) {
+
+    this.customers = [
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+      createCustomer("22637 Madison St", "St Clair Shores", "MI", "48081", "313-319-4742", "Charles"),
+    ]
+
+   }
 
   ngOnInit(): void {
+  }
+
+  createCustomer() {
+    this.modal.open(AddCustomerModalComponent)
   }
 
 }
