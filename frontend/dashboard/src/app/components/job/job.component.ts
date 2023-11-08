97ff0193b41cafe835b9f0a249931aaf66500eec
Charles Quinn <charles.quinn@planfile.com>
2023-11-08 17:47:55 -0500

    Added image processing capabilities for a job

--- frontend/dashboard/src/app/components/job/job.component.ts
+++ frontend/dashboard/src/app/components/job/job.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { WorkImagesModalComponent } from '../work-images-modal/work-images-modal.component';
+
+interface Image {
+  data: string
+  timestamp: number
+}
 
 @Component({
   selector: 'app-job',
@@ -7,9 +14,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobComponent implements OnInit {
 
-  constructor() { }
+  beforePhotos: Image[]
+  afterPhotos: Image[]
+
+  constructor(public modal: NgbModal) {
+
+    this.beforePhotos = []
+    this.afterPhotos = []
+
+  }
 
   ngOnInit(): void {
+  }
+
+  openBeforePhotos(){
+    let m = this.modal.open(WorkImagesModalComponent)
+    m.componentInstance.photos = this.beforePhotos
+    m.componentInstance.title = "Before Photos"
+  }
+
+  openAfterPhotos() {
+    let m = this.modal.open(WorkImagesModalComponent)
+    m.componentInstance.photos = this.afterPhotos
+    m.componentInstance.title = "After Photos"
   }
 
 }
