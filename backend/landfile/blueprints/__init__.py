baad028882c0797961213b089418ace9046bd6da
Charles Quinn <charles.quinn@planfile.com>
2023-11-10 16:25:56 -0500

    Added an __init__ to the blueprints package

---
+++ backend/landfile/blueprints/__init__.py
@@ -0,0 +1,19 @@
+from .account import account
+from .crew import crew
+from .crews import crews
+from .job import job
+from .jobs import jobs
+from .public import public
+from .user import user
+from .users import users
+
+BLUEPRINTS = [
+    account,
+    crew,
+    crews,
+    job,
+    jobs,
+    public,
+    user,
+    users,
+]
