d66db00f82fa874fa297dc6a65a7c2e7f268bd8f
Charles Quinn <charles.quinn@planfile.com>
2023-11-09 22:23:10 -0500

    Redid the add-crew-modal validation and layout

--- frontend/dashboard/src/app/components/add-crew-modal/add-crew-modal.component.ts
+++ frontend/dashboard/src/app/components/add-crew-modal/add-crew-modal.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
+
+interface Member {
+  email: string
+  imageJWT: string
+}
+
+
 @Component({
   selector: 'app-add-crew-modal',
   templateUrl: './add-crew-modal.component.html',
@@ -8,7 +15,13 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AddCrewModalComponent implements OnInit {
 
-  constructor(public modal: NgbActiveModal) { }
+  members: Member[]
+
+  constructor(public modal: NgbActiveModal) {
+
+    this.members = []
+
+  }
 
   ngOnInit(): void {
   }
