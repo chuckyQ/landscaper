17681f4c2467fe8675d0ac2251087284d9e78d70
Charles Quinn <charles.quinn@planfile.com>
2023-11-11 14:55:31 -0500

    Added implementation to add a user.

--- backend/landfile/blueprints/users.py
+++ backend/landfile/blueprints/users.py
@@ -9,30 +9,35 @@ users = Blueprint('users', 'users', url_prefix='/users')
 
 def get_user():
 
-    email = get_jwt_identity()
 
-    u: User = User.query.filter_by(email=email).first()
+    u: User = User.query.all()[0]
 
     if u is None:
         abort(500)
 
     assert isinstance(u, User)
 
-    if not u.is_account_admin():
+    if not u.is_admin:
         abort(403)
 
     return u
 
 
 @users.route('', methods=['GET'])
-@jwt_required
+# @jwt_required
 def get_users():
     u = get_user()
-    return [u.users]
+    return [u.json() for u in u.account.members]
 
 
 @users.route('', methods=['POST'])
-@jwt_required
-def create_crew():
+# @jwt_required
+def create_user():
+
+
+    js = request.json
 
     u = get_user()
+    acc = u.account
+    acc.add_user(email=js['email'], phone_number=js['phoneNumber'], password='abcdef', is_admin=False)
+    return {}
