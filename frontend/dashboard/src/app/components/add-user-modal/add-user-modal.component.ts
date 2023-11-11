9eeff903b7711b5e4548da2205cee1f97da324e5
Charles Quinn <charles.quinn@planfile.com>
2023-11-11 14:55:45 -0500

    Added implementation to add a user on the frontend

--- frontend/dashboard/src/app/components/add-user-modal/add-user-modal.component.ts
+++ frontend/dashboard/src/app/components/add-user-modal/add-user-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-add-user-modal',
@@ -8,13 +9,29 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AddUserModalComponent implements OnInit {
 
-  constructor(public modal: NgbActiveModal) { }
+  constructor(public modal: NgbActiveModal, public service: AuthService) { }
 
   ngOnInit(): void {
   }
 
   closeModal() {
     this.modal.close()
+  }
+
+  addUser(email: string, phoneNumber: string) {
+
+    let d = {
+      email: email,
+      phoneNumber: phoneNumber,
+    }
+    this.service.postUser(d).subscribe(
+      {
+        next: (resp: any) => {
+          alert("User added!")
+          window.location.reload()
+        }
+      }
+    )
   }
 
 }
