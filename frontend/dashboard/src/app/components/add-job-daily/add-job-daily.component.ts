cacd0f5488fd009c6db9275d84b640c365fcd12e
Charles Quinn <charles.quinn@planfile.com>
2023-11-21 23:11:04 -0500

    Modified the DailyJob interface to capture more info from a date. Removed months array (unnecessary)

--- frontend/dashboard/src/app/components/add-job-daily/add-job-daily.component.ts
+++ frontend/dashboard/src/app/components/add-job-daily/add-job-daily.component.ts
@@ -5,8 +5,19 @@ import { AuthService } from 'src/app/services/auth.service';
 interface DailyJob {
   isSeasonal: boolean // always true
   seasonalType: string // always 'daily'
+
+  startDate: string // ISO date
   startMonth: number // index of month (zero-based)
+  startDay: number // specific date of month (November 9) -> 9
+  startTimestamp: number // Unix timestamp in milliseconds
+  startWeekday: number // Weekday index (Sunday -> 1, Monday -> 2, etc.)
+
+  endDate: string // ISO date
   endMonth: number // index of month (zero-based)
+  endDay: number // specific date of month (November 9) -> 9
+  endTimestamp: number // Unix timestamp in milliseconds
+  endWeekday: number // Weekday index (Sunday -> 1, Monday -> 2, etc.)
+
   name: string // Customer name
   address: string
   custID: string
@@ -39,8 +50,6 @@ export class AddJobDailyComponent {
   @Input()
   crewIDs: string[]
 
-  months: string[]
-
   constructor(public service: AuthService, public activeModal: NgbActiveModal) {
     this.custID = ""
     this.address = ""
@@ -49,34 +58,38 @@ export class AddJobDailyComponent {
     this.notes = ""
     this.crewIDs = []
 
-    this.months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ]
   }
 
-  createDailyJob(name: string, address: string, notes: string,
-    startMonth: string, endMonth: string) {
+  createDailyJob(startDate: string, endDate: string) {
+
+    function getMonth(dt: string) {
+      let d = new Date(dt)
+      return [d.getTime(), d.getMonth(), d.getDate(), d.getDay()]
+    }
+
+    let [startTimestamp, startMonth, startDayOfMonth, startWeekday] = getMonth(startDate)
+    let [endTimestamp, endMonth, endDayOfMonth, endWeekday] = getMonth(endDate)
 
     let d: DailyJob = {
       isSeasonal: true,
       seasonalType: "daily",
-      startMonth: this.months.indexOf(startMonth),
-      endMonth: this.months.indexOf(endMonth),
-      name: name,
-      address: address,
+
+      startDate: startDate.split("T")[0],
+      startTimestamp: startTimestamp,
+      startMonth: startMonth,
+      startDay: startDayOfMonth,
+      startWeekday: startWeekday,
+
+      endDate: endDate.split("T")[0],
+      endTimestamp: endTimestamp,
+      endMonth: endMonth,
+      endDay: endDayOfMonth,
+      endWeekday: endWeekday,
+
+      name: this.custName,
+      address: this.address,
       custID: this.custID,
-      notes: notes,
+      notes: this.notes,
       crews: this.crewIDs,
     }
 
