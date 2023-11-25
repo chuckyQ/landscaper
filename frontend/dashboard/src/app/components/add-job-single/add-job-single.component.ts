f953ce71e21b5b0b01fcaa61d9de5ae768324339
Charles Quinn <charles.quinn@planfile.com>
2023-11-25 13:30:03 -0500

    Added SingleJob interface to add-job-single. Added a startDate for input. Added blank createJob method.

--- frontend/dashboard/src/app/components/add-job-single/add-job-single.component.ts
+++ frontend/dashboard/src/app/components/add-job-single/add-job-single.component.ts
@@ -1,5 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from 'src/app/services/auth.service';
+
+interface SingleJob {
+  isSeasonal: boolean // always false
+  seasonalType: string // always an empty string ''
+  dateTimestamp: number // Unix timestamp
+  name: string // Customer name
+  address: string
+  custID: string
+  crews: string[] // List of crew ids
+  notes: string
+}
 
 @Component({
   selector: 'add-job-single',
@@ -26,13 +38,21 @@ export class AddJobSingleComponent {
   @Input()
   mainFormIsValid: boolean | null
 
-  constructor(public service: AuthService) {
+  @Input()
+  startDate: string
+
+  constructor(public service: AuthService, public activeModal: NgbActiveModal) {
     this.custID = ""
     this.address = ""
     this.custName = ""
     this.notes = ""
     this.crewIDs = []
     this.mainFormIsValid = false
+    this.startDate = ""
+  }
+
+  createJob() {
+
   }
 
 }
