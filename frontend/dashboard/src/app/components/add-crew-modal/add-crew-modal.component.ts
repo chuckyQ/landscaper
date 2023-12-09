c40122ceacf8a8e91af1d552cec02dcf9fd75254
Charles Quinn <charles.quinn@planfile.com>
2023-12-08 23:21:16 -0500

    Added implemenation to add color and useBlackText in the add-crew-modal.

--- frontend/dashboard/src/app/components/add-crew-modal/add-crew-modal.component.ts
+++ frontend/dashboard/src/app/components/add-crew-modal/add-crew-modal.component.ts
@@ -9,6 +9,32 @@ interface Member {
   selected: boolean
 }
 
+interface Crew {
+  name: string
+  description: string
+  color: string
+  useBlackText: boolean
+  members: Member[]
+}
+
+/**
+ *Good info about black/white text color:
+* https://graphicdesign.stackexchange.com/questions/62368/automatically-select-a-foreground-color-based-on-a-background-color
+*/
+function useBlack(r: number, g: number, b: number) {
+  let gamma = 2.2
+  return (0.2126 * Math.pow(r / 255, gamma) +
+          0.7152 * Math.pow(g / 255, gamma) +
+          0.0722 * Math.pow(b / 255, gamma) ) > Math.pow(0.5, gamma)
+}
+
+function useBlackText(rgb: string) {
+  let rgbStr = rgb.substring(1) // Trim off the #
+  let r = Number(`0x${rgbStr.substring(0, 2)}`)
+  let g = Number(`0x${rgbStr.substring(2, 4)}`)
+  let b = Number(`0x${rgbStr.substring(4, 6)}`)
+  return useBlack(r, g, b)
+}
 
 @Component({
   selector: 'app-add-crew-modal',
@@ -44,12 +70,16 @@ export class AddCrewModalComponent implements OnInit {
     this.modal.close()
   }
 
-  addCrew(name: string, description: string) {
+  addCrew(name: string, description: string, color: string) {
 
-    let d = {
+    let useBlack = useBlackText(color)
+
+    let d: Crew = {
       name: name,
       description: description,
       members: this.members,
+      useBlackText: useBlack,
+      color: color,
     }
 
     this.service.postCrew(d).subscribe(
